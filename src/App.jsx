ee1d10e1e95ad6d2cf49233bd60ac935979d7664
afsal-code <mohammedafsalpp.3@gmail.com>
2025-03-19 12:47:19 +0530

    first commiooot

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Layout />,
-      children: [
-        { path: '/', element: <FetchData /> },
-        { path: 'detail/:id', element: <Detail /> }, // No slash at start
-      ],
-    },
-  ],
+import './App.css';
+import FetchData from './components/FetchData';
+import Detail from './components/Detail';
+import Layout from './components/Layout';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
   {
-    basename: '/multiverse-character-viewer/', // ← Add basename
-  }
-);
+    path: '/',
+    element: <Layout />,
+    children: [
+      { path: '/', element: <FetchData /> },
+      { path: '/detail/:id', element: <Detail /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+ 
+export default App;
